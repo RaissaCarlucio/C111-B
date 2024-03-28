49db946f510b3ef5937f207236f3dca49896b120
Raissa Carlucio <rabe.brasil@gmail.com>
2024-03-28 10:00:37 -0300

    Exercicio 4.5.1

--- Exercicios aula 4.5.1.py
+++ Exercicios aula 4.5.1.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 # 1 - Crie um array de floats com 10 elementos positivos e negativos entre 0 e 1. Em seguida, multiplique seus valores por 100 e crie um novo vetor apenas com a parte inteira destes numeros; (use seed(5) antes)
-# np.random.seed(5)
-# arr1 = np.random.(0,1,5) # 5 Numeros positivos
-# arr2 = -np.random.randint(0,1,5)
-# print(arr1)
-# #print(arr2)
+np.random.seed(5)
+arr1 = np.random.rand(10) # 10 Numeros aleatorios 
+arr1 = arr1*100
+arr1 = np.array(arr1, dtype=np.int64)
+print("Novo vetor: ", arr1)
 
+print("\n")
 
 # 2 - Crie uma matriz de tamanho 4x4 formada por numeros aleatorios inteiros entre 1 e 50; (use seed(10) antes)
 np.random.seed(10)
 mtz = np.random.randint(1, 50, 16).reshape(4, 4)
 print("Matris exercicio2: ", mtz)
+
 print("\n")
 
 
@@ -29,8 +31,14 @@ print("Maior valor das medias nas colunas: ", np.max(media_colunas))
 print("\n")
 
 # 4 - Baseado na matriz gerada na questao 2, mostre a quantidade de aporicoes de cada um dos numeros na mesma. Em seguida, mostre apenas os numeros que aparecem 2 vezes.
+arr3 = np.unique(mtz, return_counts=True)
+arr3 = np.array(arr3)
+print("Mostrando a matriz: ", arr3)
 
-valores_unicos, contagens = np.unique(mtz, return_counts=True)
 
-print("Valores únicos na matriz:", valores_unicos)
-print("Contagens de ocorrência de cada valor:", contagens)
+for i in range(arr3.shape[1]):
+    print("%d : %d" % (arr3[0][i], arr3[1][i]))
+
+for i in range(arr3.shape[1]):
+    if arr3[1][i] == 2:
+        print(arr3[0][i])
